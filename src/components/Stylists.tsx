f932ddc4d7e51aa881eab05066ed1a6d64cf71cf
thaingan2809 <nganthaihuhu@gmail.com>
2026-08-06 22:32:36 +0700

    Fix team section grid layout

--- src/components/Stylists.tsx
+++ src/components/Stylists.tsx
@@ -64,7 +64,7 @@ export default function Stylists() {
         </div>
 
         <div
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4"
           style={{ columnGap: '32px', rowGap: '56px' }}
         >
           {STYLISTS.map((stylist) => (
